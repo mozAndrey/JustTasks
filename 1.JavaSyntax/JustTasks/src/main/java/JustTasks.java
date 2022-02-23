eecf4d46577cf0edd1ac70ecc211b0903efcff2d
mozAndrey <1852721@mail.ru>
2022-02-23 20:18:08 +0300

    JustTasks

--- 1.JavaSyntax/JustTasks/src/main/java/JustTasks.java
+++ 1.JavaSyntax/JustTasks/src/main/java/JustTasks.java
@@ -5,12 +5,21 @@ import java.io.InputStreamReader;
 public class JustTasks {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
-        System.out.println(getSum(a));
+        System.out.println(getSum(Integer.parseInt(reader.readLine())));
 
     }
     public static int getSum (int a) {
+        int result = 0;
+        if (a > 0) {
+            for (int i = 0; i <= a; i++) {
+                result += i;
+            }
+        } else {
+            for (int i = a; i <= 1; i++) {
+                result -= i;
 
-        return ((1+a)/2)*a;
+            }
+        }
+        return result;
     }
 }
